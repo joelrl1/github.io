25bfd8b10ab2ca684f149602f3e17d4faf31dbff
Joel <joel1.robert@gmail.com>
2022-04-07 15:58:07 +0200

    coords

--- nz/main.js
+++ nz/main.js
@@ -1,14 +1,20 @@
 //Portugalreise Skript
-let lat = 38.716667
-let lng= -9.166667
-let zoom= 13
-let map = L.map('map').setView([lat, lng], zoom);
+let lat = 38.716667;
+let lng= -9.166667;
+let zoom= 13;
+
+let coords= [38.716667, -9.166667];
+console.log(coords);
+console.log(coords[0]);
+console.log(coords[1]);
+console.log(coords.length);
+let map = L.map('map').setView(coords, zoom);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([lat, lng]).addTo(map)
+L.marker(coords).addTo(map)
     .bindPopup("<h3> Lissabon </h3>")
     .openPopup(); 
 
