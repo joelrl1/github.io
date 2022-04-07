362c49e424f6522771429b3de5f413dec57478ea
Joel <joel1.robert@gmail.com>
2022-04-07 15:40:40 +0200

    koordinaten durch variabelen ersetzt

--- nz/main.js
+++ nz/main.js
@@ -1,11 +1,14 @@
 //Portugalreise Skript
-
-var map = L.map('map').setView([38.716667, -9.166667], 13);
+let lat = 38.716667
+let lng= -9.166667
+let zoom= 13
+let map = L.map('map').setView([lat, lng], zoom);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([38.716667, -9.166667]).addTo(map)
+L.marker([lat, lng]).addTo(map)
     .bindPopup("<h3> Lissabon </h3>")
     .openPopup(); 
+
